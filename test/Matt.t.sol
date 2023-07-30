980d38168d874518c84960c55d9a3d4d4fa18658
Dan Finlay <dan.finlay@consensys.net>
2023-07-30 16:33:03 -0700

    Tests passing

--- test/Matt.t.sol
+++ test/Matt.t.sol
@@ -3,20 +3,40 @@ pragma solidity ^0.8.10;
 import "forge-std/Test.sol";
 import "../src/Matt.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(address to, uint256 amount) ERC20("MockERC20", "M20") {
+        _mint(to, amount);
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
 
 contract MattTest is Test {
     Matt matt;
-    IERC20 auctionCurrency;
+    MockERC20 auctionCurrency;
     address[] buyers;
     uint256 price;
 
     function setUp() public {
-        auctionCurrency = IERC20(address(this)); // You can replace with a proper mock ERC20
+        auctionCurrency = new MockERC20(address(this), 10000);
         matt = new Matt(address(auctionCurrency), "https://example.com/", "SimpleNFT", "SNFT");
-        buyers = [address(1), address(2)]; // Replace with actual buyer addresses
+        buyers = [address(1), address(2)]; 
         price = 100;
 
-        // Assume the contract has enough balance of the auction currency
+        for (uint256 i = 0; i < buyers.length; i++) {
+            auctionCurrency.mint(buyers[i], price + 100);
+            
+            // Prank to set the sender to the buyer's address
+            vm.prank(buyers[i]);
+            require(auctionCurrency.approve(address(matt), price + 100), "Approval failed");
+        }
+        
+        // Resetting the sender after the loop
+        vm.prank(address(0));
     }
 
     function test_TransfersAmountFromEachBuyer() public {
